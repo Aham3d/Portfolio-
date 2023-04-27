c61bf0b63fd7d531655afe63464d51940d59b137
Moin Ahamed <ahamedjust@gmail.com>
2023-04-27 23:03:12 +0530

    form validation and cursor code copied to js file

---
+++ index.js
@@ -0,0 +1,77 @@
+// CURSOR
+console.clear();
+
+const TAIL_LENGTH = 20;
+
+const cursor = document.getElementById("cursor");
+
+let mouseX = 0;
+let mouseY = 0;
+
+let cursorCircles;
+let cursorHistory = Array(TAIL_LENGTH).fill({ x: 0, y: 0 });
+
+function onMouseMove(event) {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+}
+
+function initCursor() {
+  for (let i = 0; i < TAIL_LENGTH; i++) {
+    let div = document.createElement("div");
+    div.classList.add("cursor-circle");
+    cursor.append(div);
+  }
+  cursorCircles = Array.from(document.querySelectorAll(".cursor-circle"));
+}
+
+function updateCursor() {
+  cursorHistory.shift();
+  cursorHistory.push({ x: mouseX, y: mouseY });
+
+  for (let i = 0; i < TAIL_LENGTH; i++) {
+    let current = cursorHistory[i];
+    let next = cursorHistory[i + 1] || cursorHistory[TAIL_LENGTH - 1];
+
+    let xDiff = next.x - current.x;
+    let yDiff = next.y - current.y;
+
+    current.x += xDiff * 0.35;
+    current.y += yDiff * 0.35;
+    cursorCircles[i].style.transform = `translate(${current.x}px, ${
+      current.y
+    }px) scale(${i / TAIL_LENGTH})`;
+  }
+  requestAnimationFrame(updateCursor);
+}
+
+document.addEventListener("mousemove", onMouseMove, false);
+
+initCursor();
+updateCursor();
+
+// FORM VALIDATION
+const form = document.querySelector('#form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const messageInput = document.querySelector('#message');
+
+const namePattern = /^[a-zA-Z]{3,}$/;
+const emailPattern = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/;
+const messagePattern = /^[a-zA-Z]{3,}$/;
+
+function validateForm(event) {
+  event.preventDefault();
+  let isValid = true;
+  if (isValid) {
+    console.log('Message sent');
+    console.log(nameInput.value);
+    console.log(emailInput.value);
+    console.log(messageInput.value);
+    form.reset();
+  } else {
+    console.log('message not sent');
+  }
+}
+
+form.addEventListener('submit', validateForm);
